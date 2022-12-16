6b05a524591d47dca83a596c2595f6a62083a794
abubakar7879 <93710180+abubakar7879@users.noreply.github.com>
2022-12-16 17:18:57 +0500

    Initial Project Commit

---
+++ PandemicMonitoringSystem/PandemicMonitoringSystem/Abstraction/IDbStratrgy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PandemicMonitoringSystem.Abstraction
+{
+    public interface IDbStratrgy
+    {
+        // seeding the database with necessary schema and data changes
+        void SeedDatabase();
+        // Get Connection to the Db
+        DbConnection Connection { get; }
+    }
+}
---
+++ PandemicMonitoringSystem/PandemicMonitoringSystem/Data/SqlDatabaseStrategy.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using PandemicMonitoringSystem.Abstraction;
+
+namespace PandemicMonitoringSystem.Data
+{
+    public class SqlDatabaseStrategy : IDbStratrgy
+
+    {
+        public DbConnection Connection
+        {
+            get
+            {
+                string projectPath = AppDomain.CurrentDomain.BaseDirectory.Split(new String[] { @"bin\" }, StringSplitOptions.None)[0];
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(projectPath)
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                string connectionString = configuration.GetConnectionString("DefaultConnection");
+                if (_connection == null)
+                {
+                    _connection = new SqlConnection(connectionString);
+                    _connection.Open();
+                }
+                else if (_connection.State != ConnectionState.Open)
+                {
+                    _connection.Open();
+                }
+                return _connection;
+            }
+        }
+        private DbConnection _connection;
+        public void SeedDatabase()
+        {
+
+        }
+    }
+}
